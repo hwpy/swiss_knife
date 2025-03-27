412c27d195030b48e40d9e828c1a17d1f0b355fa
hwpy <photodvfusr1@gmail.com>
2025-03-28 01:18:20 +1000

    документирование кода

--- scripts/build_tools.py
+++ scripts/build_tools.py
@@ -1,3 +1,10 @@
+"""Название: build_tools
+
+Автор: hwpy
+Дата: 2025-03-28
+Описание: Функционал сборки приложения под разные системы
+"""
+
 import ast
 import subprocess
 import sys
@@ -6,34 +13,52 @@ from pathlib import Path
 
 from jinja2 import Template
 
-current_dir = Path(getcwd())
+current_dir = Path(getcwd())  # noqa: PTH109
 sys.path.append(str(current_dir))
+
 from config.swiss_knife_config import JinjaTemplates, SwissKnifeConfig
 from utils.platform_settings import Platform
 
 
 def pick_builder_template_by_os(config: SwissKnifeConfig) -> dict:
-    if Platform.OS == Platform.Windows:
+    """Выбрать шаблон сборки для ОС
+
+    Args:
+        config: SwissKnifeConfig - конфиг приложения
+
+    Returns:
+       dict[str: dict[str: str]] - {строка сборки: параметры сборки}
+
+    """
+    if Platform.Windows == Platform.OS:
         params = {
             "app_name": config.build_params["app_name"],
             "win_icon_path": config.build_params["win_icon_path"],
             "app_py": config.build_params["app_py"],
         }
         return {"builder": JinjaTemplates.pyinstaller_win, "params": params}
-    elif Platform.OS == Platform.macOS:
+    if Platform.macOS == Platform.OS:
         params = {
             "app_name": config.build_params["app_name"],
             "mac_icon_path": config.build_params["mac_icon_path"],
             "app_py": config.build_params["app_py"],
         }
         return {"builder": JinjaTemplates.pyinstaller_mac, "params": params}
-    else:
-        raise SystemError("Данная ОС на текущий момент не поддерживается!")
+    raise SystemError("Данная ОС на текущий момент не поддерживается!")
 
 
 def build_with_pyinstaller(config: SwissKnifeConfig) -> None:
+    """Собрать приложение с помощью pyinstaller
+
+    Args:
+        config: SwissKnifeConfig - конфиг приложения
+
+    Returns:
+       None
+
+    """
     builder_template = pick_builder_template_by_os(config)
-    with open(builder_template["builder"], "r") as f:
+    with open(builder_template["builder"]) as f:
         template_str = f.read()
         template = Template(template_str)
         command = template.render(builder_template["params"])
@@ -45,6 +70,15 @@ def build_with_pyinstaller(config: SwissKnifeConfig) -> None:
 
 
 def build_readme(config: SwissKnifeConfig) -> None:
+    """Собрать readme
+
+    Args:
+        config: SwissKnifeConfig - конфиг приложения
+
+    Returns:
+        None
+
+    """
     with open(JinjaTemplates.readme) as f:
         template_str = f.read()
 
